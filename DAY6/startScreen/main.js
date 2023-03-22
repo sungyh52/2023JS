c8fdfff24c7f9d22d564d186adab5a6f241c3fbc
sungyh52 <sungyh5252@gmail.com>
2023-03-22 18:09:41 +0900

    startScreen

---
+++ DAY6/startScreen/main.js
@@ -0,0 +1,15 @@
+function getTime() {
+  const time = document.querySelector(".time");
+
+  const newDate = new Date();
+
+  const hours = String(newDate.getHours()).padStart(2, "0");
+  const minutes = String(newDate.getMinutes()).padStart(2, "0");
+  const seconds = String(newDate.getSeconds()).padStart(2, "0");
+
+  // time.innerText = hours + ":" + minutes + ":" + seconds;
+  time.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
